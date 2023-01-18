185feac251218b18a2d2ed6235faa47a32cbf13f
G-RIMA <gikandimaria178@gmail.com>
2023-01-18 13:02:11 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 import requests
-import redis
+import time
+from functools import wraps
+from typing import Dict
 
-#create redis connection
-r = redis.StrictRedis()
+cache: Dict[str, str] = {}
 
 def get_page(url: str) -> str:
-    #check if we have a cached result for this url
-    cache_key = "count:" + url
-    cached_result = r.get(cache_key)
-    if cached_result:
-        r.incr(cache_key)
-        return cached_result.decode()
+    if url in cache:
+        print(f"Retrieving from cache: {url}")
+        return cache[url]
     else:
-        #if no cached result, request the page and cache the result
-        result = requests.get(url)
-        r.set(cache_key, result.text, ex=10)
-        r.incr(cache_key)
-        return result.text
+        print(f"Retrieving from web: {url}")
+        response = requests.get(url)
+        result = response.text
+        cache[url] = result
+        return result
 
+def cache_with_expiration(expiration: int):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            url = args[0]
+            key = f"count:{url}"
+            if key in cache:
+                count, timestamp = cache[key]
+                if time.time() - timestamp > expiration:
+                    result = func(*args, **kwargs)
+                    cache[key] = (count+1, time.time())
+                    return result
+                else:
+                    cache[key] = (count+1, timestamp)
+                    return
