ac1374ce4bff0799167cf9841b0db7069f1610b7
G-RIMA <gikandimaria178@gmail.com>
2023-01-21 11:56:15 +0300

    fixing codes

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ pymongo
 """
+from pymongo import ASCENDING
 
 def top_students(mongo_collection):
+    # calculate the average score for each student
     pipeline = [
-        {"$unwind": "$scores"},
-        {"$group": {"_id": "$_id", "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": -1}}
+        {"$group": {"_id": "$student_id", "averageScore": {"$avg": "$score"}}},
+        {"$project": {"student_id": "$_id", "averageScore": 1}},
+        {"$sort": {"averageScore": ASCENDING}}
     ]
-    return list(mongo_collection.aggregate(pipeline))
+    students = list(mongo_collection.aggregate(pipeline))
+    return students
